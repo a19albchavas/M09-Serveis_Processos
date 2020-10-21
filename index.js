be81893d6a72c85809da9a1c38723900515f6919
Albert <72222514+a19albchavas@users.noreply.github.com>
2020-10-21 17:12:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,35 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json())
 
 let jugador = {
+    posicio: '',
+    alies: '',
     nombre: '',
     apellido: '',
     score: ''
 };
+
+let jugadores = [{
+    posicio: 3,
+    alies: "jperez",
+    nom: "Jose",
+    congnom: "Perez",
+    score: 1000
+},
+{
+    posicio: 1,
+    alies: "jsanz",
+    nom: "Juan",
+    congnom: "Sanz",
+    score: 950
+},
+{
+    posicio: 1,
+    alies: "mgutierrez",
+    nom: "Maria",
+    congnom: "Gutierrez",
+    score: 850
+}
+];
 
 let respuesta = {
     error: false,
@@ -66,6 +91,18 @@ app.post('/gamer', function (req, res) {
         }
     }
     res.send(respuesta);
+});
+
+
+
+app.get('/ranking', function (req, res)
+{
+    jugadores.sort((a, b) => (a.score < b.score ? 1:-1));
+    for (i = 0; i < jugadores.length; i++)
+    {
+        jugadores[i].posicio = i + 1;
+    }
+    res.send(jugadores);
 });
 
 app.get('/hola', function (req, res) {
