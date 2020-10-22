75b8cfa0fea1609b4e78ae589f3cf5456c2e8adf
Albert <72222514+a19albchavas@users.noreply.github.com>
2020-10-22 20:30:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,48 +53,6 @@ app.get('/', function (req, res) {
     res.send(respuesta);
 });
 
-app.post('/gamer', function (req, res) {
-    var nom = req.body.nombre || null;
-    var cognom = req.body.apellido || null;
-    var puntuacio = req.body.score || null;
-    if (nom == null || cognom == null || puntuacio == null)
-    {
-        respuesta = {
-            error: true,
-            codigo: 502,
-            mensaje: "El campo nombre, apellido y score son requeridos"
-        }
-    }
-    else
-    {
-        if (jugador.nombre == nom && jugador.apellido == cognom)
-        {
-            respuesta = {
-                error: true,
-                codigo: 503,
-                mensaje: "El jugador ya fue creado previamente"
-            }
-        }
-        else
-        {
-            jugador = {
-                nombre: nom,
-                apellido: cognom,
-                score: puntuacio
-            };
-            respuesta = {
-                error: false,
-                codigo: 200,
-                mensaje: 'Jugador creado',
-                gamer: jugador
-            }
-        }
-    }
-    res.send(respuesta);
-});
-
-
-
 app.get('/ranking', function (req, res)
 {
     jugadores.sort((a, b) => (a.score < b.score ? 1:-1));
@@ -104,6 +62,7 @@ app.get('/ranking', function (req, res)
     }
     res.send(jugadores);
 });
+
 
 app.get('/hola', function (req, res) {
     res.send('[GET]Hola');
