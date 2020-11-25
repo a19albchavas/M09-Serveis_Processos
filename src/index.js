decd7ad2a04497405a2549a5925540efce7a033a
Albert <72222514+a19albchavas@users.noreply.github.com>
2020-11-25 16:40:37 +0100

    entrega2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 apijs.listen(port, () => {
-    console.log("El servidor está inicializado en el puerto "+ port);
+    console.log("El servidor está inicializado en el puerto " + port);
 });
 
 
